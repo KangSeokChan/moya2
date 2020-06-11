b5815663922fe05af7fc046368e62cc3cd5df6cf
KangSeokChan <lucky3577@naver.com>
2020-06-11 20:22:57 +0900

    6.11

---
+++ app/src/main/java/com/example/moya2/Tab1layout.java
@@ -0,0 +1,18 @@
+package com.example.moya2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Tab1layout extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+       return inflater.inflate(R.layout.tab1_layout,container,false);
+    }
+}
---
+++ app/src/main/java/com/example/moya2/Tip1.java
@@ -0,0 +1,13 @@
+package com.example.moya2;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Tip1 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tip1);
+    }}
